29a20a2fb659612053ce7e6391d71e81ad7c5959
Priyank Gohil <priyank.gohil@levaral.com>
2018-12-06 15:03:20 +0530

    Update heroku url

--- public/autoupdater.js
+++ public/autoupdater.js
@@ -4,7 +4,7 @@ const {app, autoUpdater, dialog} = require('electron');
 const version = app.getVersion();
 const platform = os.platform() + '_' + os.arch();  // usually returns darwin_64
 
-const updaterFeedURL = 'http://zulipdesktop.herokuapp.com/update/' + platform + '/' + version;
+const updaterFeedURL = 'http://deribit.herokuapp.com/update/' + platform + '/' + version;
 // replace updaterFeedURL with http://yourappname.herokuapp.com
 
 function appUpdater() {
